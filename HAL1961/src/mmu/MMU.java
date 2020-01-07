690b3a17208264516566fc4bf1391197fb7b9113
WarhawkPrime <dennis.wilpert@web.de>
2020-01-07 16:09:15 +0100

    tfdgd

--- HAL1961/src/mmu/MMU.java
+++ HAL1961/src/mmu/MMU.java
@@ -22,7 +22,7 @@ public class MMU {
 	private short pageReg3;
 	
 	//dies ist ein test
-	
+	//trest2
 	private short register0;			          
 	private short register1;			
 	private short register2;			                                             
